57a000b8a9f8c26da636191742f574d8b2f4f308
salis2 <56523603+salis2@users.noreply.github.com>
2020-12-22 09:48:47 +0700

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string Insert(InsertUser user);
+
+        [OperationContract]
+        gettestdata GetInfo();
+
+        [OperationContract]
+        string Update(UpdateUser u);
+
+        [OperationContract]
+        string Delete(DeleteUser d);
+
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class DeleteUser
+    {
+        int uid;
+        [DataMember]
+        public int UID
+        {
+            get { return uid; }
+            set { uid = value; }
+        }
+    }
+
+    [DataContract]
+    public class gettestdata
+    {
+        [DataMember]
+        public DataTable usertab { get; set; }
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibrary1.ContractType".
+    [DataContract]
+    public class InsertUser
+    {
+        string name = string.Empty;
+        string email = string.Empty;
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+
+    [DataContract]
+    public class UpdateUser
+    {
+        int uid;
+        string name;
+        string email;
+        [DataMember]
+        public int UID
+        {
+            get { return uid; }
+            set { uid = value; }
+        }
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        [DataMember]
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+
+    
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 :  IService1
+    {
+        public string Insert(InsertUser user)
+        {
+            string msg;
+            SqlConnection con = new SqlConnection("Server = Salis-R; Database = teswcf2; user Id = sa; password = 1234; Trusted_Connection = False; MultipleActiveResultSets = true; ");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Insert Into UserTab(Name, Email) Values(@Name, @Email)", con);
+            cmd.Parameters.AddWithValue("@Name", user.Name);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+
+            int g = cmd.ExecuteNonQuery();
+            if (g == 1)
+            {
+                msg = "Successfully Inserted";
+            }
+            else
+            {
+                msg = "Failed to Insert";
+            }
+            return msg;
+        }
+        public gettestdata GetInfo()
+        {
+            gettestdata g = new gettestdata();
+            SqlConnection con = new SqlConnection("Server = Salis-R; Database = teswcf2; user Id = sa; password = 1234; Trusted_Connection = False; MultipleActiveResultSets = true; ");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from UserTab", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("UserTab");
+            da.Fill(dt);
+            g.usertab = dt;
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return g;
+
+        }
+
+        public string Update(UpdateUser u)
+        {
+            string messege;
+            SqlConnection con = new SqlConnection("Server = Salis-R; Database = teswcf2; user Id = sa; password = 1234; Trusted_Connection = False; MultipleActiveResultSets = true; ");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update UserTab set Name = @Name, Email = @Email where UserID = @UserID", con);
+            cmd.Parameters.AddWithValue("@UserID", u.UID);
+            cmd.Parameters.AddWithValue("@Name", u.Name);
+            cmd.Parameters.AddWithValue("@Email", u.Email);
+            int res = cmd.ExecuteNonQuery();
+            if (res == 1)
+            {
+                messege = "Successfully Updated";
+            }
+            else
+            {
+                messege = "Failed to Updated";
+            }
+            return messege;
+        }
+
+        public string Delete(DeleteUser d)
+        {
+            string msg = "";
+            SqlConnection con = new SqlConnection("Server = Salis-R; Database = teswcf2; user Id = sa; password = 1234; Trusted_Connection = False; MultipleActiveResultSets = true; ");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete UserTab where UserID = @UserID", con);
+            cmd.Parameters.AddWithValue("@USerId", d.UID);
+            int res = cmd.ExecuteNonQuery();
+            if (res == 1)
+            {
+                msg = "Successfully Deleted";
+            }
+            else
+            {
+                msg = "Failed to Deleted";
+            }
+            return msg;
+        }
+
+
+    }
+}
